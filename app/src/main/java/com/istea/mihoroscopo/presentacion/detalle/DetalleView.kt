ec5eb27f888c3497689c108adabcced0754bc571
revrad <madeonico@gmail.com>
2024-07-02 19:29:24 -0300

    TopBar Color 2

--- app/src/main/java/com/istea/mihoroscopo/presentacion/detalle/DetalleView.kt
+++ app/src/main/java/com/istea/mihoroscopo/presentacion/detalle/DetalleView.kt
@@ -51,8 +51,8 @@ fun DetalleView (
         topBar = {
             TopAppBar(
                 colors = TopAppBarDefaults.topAppBarColors(
-                    containerColor = MaterialTheme.colorScheme.primaryContainer,
-                    titleContentColor = MaterialTheme.colorScheme.primary
+                    containerColor = MaterialTheme.colorScheme.secondaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.secondary
                 ),
                 title = { Text(text = "Horoscopo") },
                 navigationIcon = {
